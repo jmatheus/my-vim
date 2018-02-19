0662f39e75398b9ac6da35167dff469c181a763d
Julio Matheus <jmatheus.vaz@gmail.com>
2018-02-19 18:04:58 -0300

    add start script

---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ln -sf /home/julio/projects/my-vim/vimrc ~/.vimrc
+ln -sf /home/julio/projects/my-vim/vim/ ~/.vim
+
+
+git clone https://github.com/vim-airline/vim-airline vim/bundle/vim-airline #vim-airline
+git clone git://github.com/jiangmiao/auto-pairs.git vim/bundle/auto-pairs #auto-pairs
+git clone https://github.com/kien/ctrlp.vim.git vim/bundle/ctrlp.vim #ctrlp
+git clone https://github.com/morhetz/gruvbox.git vim/bundle/gruvbox #gruvbox
+git clone https://github.com/scrooloose/nerdtree.git vim/bundle/nerdtree #nerdtree
+git clone https://github.com/ervandew/supertab vim/bundle/supertab #supertab
+git clone https://github.com/tpope/vim-endwise vim/bundle/vim-endwise #vim-endwise
+git clone https://github.com/fatih/vim-go.git vim/pack/plugins/start/vim-go #vim-go
+git clone https://github.com/slim-template/vim-slim vim/bundle/vim-slim #vim-slim
+
+
+
+
